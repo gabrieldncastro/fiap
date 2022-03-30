4c8f63d9dde5cafdf604792c51279e0d6ec06752
gabrieldncastro <gabrielncastro@outlook.com>
2022-03-30 14:52:14 -0300

    commit

---
+++ tipos_variaveis.py
@@ -0,0 +1,8 @@
+
+ano = 1989
+nome = "Luke Skywalker"
+saldo = 50.30
+
+print('O tipo de variável ano é {}'.format(type(ano)))
+print('O tipo de variável nome é {}'.format(type(nome)))
+print('O tipo de variável nome é {}'.format(type(saldo)))
