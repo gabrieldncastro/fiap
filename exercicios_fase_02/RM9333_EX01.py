ebe095766bcc1af1aaa186020cbdd60922a95377
gabrieldncastro <gabrielncastro@outlook.com>
2022-04-06 23:36:29 -0300

    commit

--- exercicios_fase_02/RM9333_EX01.py
+++ exercicios_fase_02/RM9333_EX01.py
@@ -1,18 +1,26 @@
+#exercicio 01
 
 annualInvoice = float(input('\nDigite o valor do seu faturamento anual em reais: '))
 planType = int(input('\nSelecione abaixo o seu atual plano de assinatura:\n\n [1] Basic | [2] Silver | [3] Gold | [4] Platinum\n\nPlano de assinatura: '))
-paymantPorcentage = 0
+
+paymantPercentage = 0
 
 if planType == 1:
-    paymantPorcentage = annualInvoice * 0.30
+    paymantPercentage = annualInvoice * 0.30
 elif planType == 2:
-    paymantPorcentage = annualInvoice * 0.20
+    paymantPercentage = annualInvoice * 0.20
 elif planType == 3:
-    paymantPorcentage = annualInvoice * 0.10
+    paymantPercentage = annualInvoice * 0.10
 elif planType == 4:
-    paymantPorcentage = annualInvoice * 0.05
+    paymantPercentage = annualInvoice * 0.05
 else:
     print('Essa não é uma opção válida')
 
-if paymantPorcentage != 0:
-    print(f'O valor a ser pago para o locatário é {paymantPorcentage:.2f}')
+    
+if paymantPercentage != 0:
+    print(f'O valor a ser pago para o locatário é: {paymantPercentage:.2f} reais')
+
+#adicionar uma estrutura de repetição para que, caso o usuário queira, retornar para opção de digitar salário
+#adicionar estrutura de repetição para, quando não for uma opção válida, retornar as opções de plano de assinatura
+
+  
--- exercicios_fase_02/RM9333_EX02 copy.py
+++ exercicios_fase_02/RM9333_EX02 copy.py
@@ -1,4 +1,8 @@
-#exercicio 02
+"""
+Os alunos da sua turma fizeram uma votação para escolher qual dia da semana é o melhor para a realização das lives.
+Desenvolva um programa em que o usuário informe a quantidade de votos que cada um dos 5 dias da semana
+(segunda-feira, terça-feira, quarta-feira, quinta-feira e sexta-feira) obtiveram, verifique e exiba qual dia foi o escolhido. 
+"""
 
 print('\nPor favor, digite a quantidade total de votos que cada dia da semana recebeu\n')
 
