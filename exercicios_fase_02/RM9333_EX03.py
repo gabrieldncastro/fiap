123a38a694a816313852dfa9cdd486afdb979544
gabrieldncastro <gabrielncastro@outlook.com>
2022-04-11 14:57:35 -0300

    commit

--- exercicios_fase_02/RM9333_EX03.py
+++ exercicios_fase_02/RM9333_EX03.py
@@ -1,35 +1,40 @@
 #exercicio 03
 
 #variáveis
-estudante = 1
+studant = 1
+oddNotes = 0
+totalOddNote = 0
+evenNotes = 0
+totalEvenNote = 0
 
-notaImpar = 0
-estudanteImpar = 0
-mediaImpar = 0
-
-notaPar = 0
-estudantePar = 0
-mediaPar = 0
-
-#alunos quem tem o numero impar na chamada
+#alunos quem tem o numero ímpar na chamada
 print('\nAtenção, você está digitando a nota dos alunos ímpares\n')
 
-while (estudante <= 10):
-    if estudante % 2 == 1:
-        notaImpar = float(input(f'Digite a nota do estudante nº{estudante}: '))
-        mediaImpar = mediaImpar + notaImpar
-        estudanteImpar = estudanteImpar + 1
-    estudante = estudante + 1
-estudante = 0
+while (studant <= 50):
+    if studant % 2 == 1:
+        oddNotes = float(input(f'Digite a nota do studant nº{studant}: '))
+        totalOddNote = totalOddNote + oddNotes
+    studant = studant + 1
+studant = 0
 
 #alunos que tem o numero par na chamada
 print('\nAtenção, você está digitando a nota dos estudantes pares\n')
-while (estudante <= 10):
-    if estudante % 2 == 0 and estudante > 0:
-        notaPar = float(input(f'Digite a nota do estudante nº{estudante}: '))
-        mediaPar = mediaPar + notaPar
-        estudantePar = estudantePar + 1
-    estudante = estudante + 1
-estudante = 0
+while (studant <= 50):
+    if studant % 2 == 0 and studant > 0:
+        evenNotes = float(input(f'Digite a nota do studant nº{studant}: '))
+        totalEvenNote = totalEvenNote + evenNotes
+    studant = studant + 1
+studant = 0
 
-#print(f'\n A média dos estudantes foi:\n Estudantes Impares: {:.1f} | Estudantes Par:{:.1f}')
+#medias
+averageEvenStudants = totalEvenNote / 25
+averageOddStudants = totalOddNote / 25
+
+print(f'\n A média dos estudantes foi:\n\n Estudantes ímpares: {averageOddStudants:.1f} | Estudantes Par:{averageEvenStudants:.1f}')
+
+if averageOddStudants == averageEvenStudants:
+    print('\nHouve empate entre a média dos alunos ímpares e pares')
+elif averageOddStudants > averageEvenStudants:
+    print(f'\nOs alunos com a média ímpar obtiveram uma média superior aos alunos com numero par na chamada.')
+else:
+    print(f'\nOs alunos com a média Par obtiveram uma média superior aos alunos com ímpar na chamada.')
